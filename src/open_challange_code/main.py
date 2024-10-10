4b2bda4c8329c9f7318e0755df333413ac382f9c
mumuniazeez <mumuniazeez99@gmail.com>
2024-10-10 16:46:44 +0100

    "Updated VSCode settings and added robot control code to main.py"

--- src/open_challange_code/main.py
+++ src/open_challange_code/main.py
@@ -15,6 +15,56 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 
+left_ultrasonic = UltrasonicSensor(Port.S1)
+right_ultrasonic = UltrasonicSensor(Port.S2)
+bottom_color = ColorSensor(Port.S3)
+
+steering = Motor(Port.A)
+left_motor = Motor(Port.B, gears=[20, 12])
+right_motor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE, gears=[20, 12])
+
 
 # Write your program here.
 ev3.speaker.beep()
+
+steering.run_time(-500, 500, Stop.HOLD)
+wait(100)
+steering.run_angle(5000, 90)
+wait(100)
+steering.reset_angle(0)
+
+
+# wait(2000000)
+
+# right_ultrasonic.distance() < 1000
+
+
+while True:
+    while not bottom_color.rgb()[0] < 10:
+
+        if right_ultrasonic.distance() > 0 and right_ultrasonic.distance() < 200:
+            steering.run_angle(500, -65 - steering.angle())
+            print("very close")
+
+        elif right_ultrasonic.distance() < 400:
+            steering.run_angle(500, -40 - steering.angle())
+            print("Too close")
+
+        if right_ultrasonic.distance() > 460 and right_ultrasonic.distance() < 3000:
+            steering.run_angle(500, 65 - steering.angle())
+            print("very far")
+
+        elif right_ultrasonic.distance() > 450:
+            steering.run_angle(500, 40 - steering.angle())
+            print("Too far")
+
+        
+        if right_ultrasonic.distance() > 400 and right_ultrasonic.distance() < 450:
+            steering.run_angle(500, 0 - steering.angle())
+            print("good")
+        
+
+        left_motor.run(1000)
+        right_motor.run(1000)
+        
+    ev3.speaker.beep()
